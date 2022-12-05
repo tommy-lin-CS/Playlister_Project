04a2911e212874a311bc12a8c35fbf6ca6d11c0c
Tommy <lin.tommy@outlook.com>
2022-12-05 03:37:45 -0500

    Removed Status Bar at Splash Screen

--- client/src/components/Statusbar.js
+++ client/src/components/Statusbar.js
@@ -1,6 +1,7 @@
 import { useContext } from 'react'
 import { GlobalStoreContext } from '../store'
 import { Typography } from '@mui/material'
+import AuthContext from '../auth'
 
 /*
     Our Status bar React component goes at the bottom of our UI.
@@ -9,15 +10,23 @@ import { Typography } from '@mui/material'
 */
 function Statusbar() {
     const { store } = useContext(GlobalStoreContext);
+    const { auth } = useContext(AuthContext);
+
     let text ="";
-    if (store.currentList)
+    
+    if (auth.loggedIn) {
+        if (store.currentList)
         text = store.currentList.name;
         return (
             <div id="playlister-statusbar">
                 <Typography variant="h4">{text}</Typography>
             </div>
         );
-  
+    }
+    else {
+        return null;
+    }
+    
     
 }
 
